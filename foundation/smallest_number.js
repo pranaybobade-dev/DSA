f206052acf5aa23b0ffbf8c426eca3cf95ffea4e
pranaybobade-dev <pranaybobade.dev@gmail.com>
2025-11-23 20:02:29 +0530

    remove Duplicates

---
+++ foundation/smallest_number.js
@@ -0,0 +1,13 @@
+function findSmallest(arr) {
+  let smallest = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+    }
+  }
+  return smallest;
+}
+
+let arr = [2, -6, 4, 8, 1, -9];
+let result = findSmallest(arr);
+console.log("Result:", result);
