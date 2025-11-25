23c0cef93b0996b08169dd20d2272f1b1908c783
pranaybobade-dev <pranaybobade.dev@gmail.com>
2025-11-25 21:30:18 +0530

    Single Number

---
+++ Arrays/single_number.js
@@ -0,0 +1,21 @@
+let arr = [1, 3, 3, 4, 4, 5, 6, 6, 5];
+
+function singleNumber(arr) {
+  let hashMap = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!hashMap[arr[i]]) {
+      hashMap[arr[i]] = 1;
+    } else {
+      hashMap[arr[i]]++;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (hashMap[arr[i]] === 1) {
+      return hashMap[arr[i]];
+    }
+  }
+}
+
+
+console.log(singleNumber(arr));
